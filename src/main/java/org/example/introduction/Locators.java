0a669078240a6d6c6cde5938f358e632520a8090
William <whellems@msudenver.edu>
2024-03-18 02:54:53 -0600

    Synchronization code & locate elements

--- src/main/java/org/example/introduction/Locators.java
+++ src/main/java/org/example/introduction/Locators.java
@@ -4,14 +4,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Locators {
     public static void main(String[] args) {
         // Chrome
         System.setProperty("webdriver.chrome.driver", "/Users/williamhellems-moody/Documents/selenium-drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
-        // Type the search query into the search box using the class name locator
-        driver.findElement(By.className("gLFyf")).sendKeys("What is Selenium");
 
+        // Implicit Wait - Selenium waits for 5 seconds before throwing an exception. Assist with synchronization issues.
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("<ENTER URL>");
+
+        // Locators
+        driver.findElement(By.id("inputUsername")).sendKeys("william");
+        driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+        driver.findElement(By.className("signInBtn")).click();
+
+        /*
+        * Selenium scans from top to bottom!!
+        *
+        * Can use CSS and Xpath
+        * - CSS
+        * -- Tag and ID
+        *       id -> tagname#id -> input#inputUsername
+        * If no class or id, use tagname and attribute
+        *      input[name='inputPassword']
+        * -- Construct a CSS selector using class to identify the element
+        *       input[placeholder="Username"]
+        * */
+
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText()); // extract text from error message
     }
 }
